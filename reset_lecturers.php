764557f87143d750642eace37b23cda194f04e27
Abdul <bambakudus@gmail.com>
2026-06-05 18:43:51 +0000

    remove: lecturer reset script

--- reset_lecturers.php
+++
@@ -1,8 +0,0 @@
-<?php
-$key = $_GET['key'] ?? '';
-if ($key !== 'citadel_reset_lec_2026') { http_response_code(403); die('No'); }
-require_once 'includes/db.php';
-$hash = '$2y$10$pqu3wDFTVeCzGl2Rnq7ORe3E8qp8yuTsul/1WeZQHh0s/8xZVtOoe';
-$stmt = $pdo->prepare("UPDATE users SET password_hash=? WHERE role='lecturer'");
-$stmt->execute([$hash]);
-echo "Updated " . $stmt->rowCount() . " lecturers. Password is now: Lecturer@123";
